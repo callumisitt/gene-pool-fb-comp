6fda024b1759dbd52f6f1cbf5651d8a5185fd3c5
Callum Isitt <callum@callumisitt.co.uk>
2017-07-10 19:36:48 +0100

    Verified if winner has like the page

--- gene_pool_comp.rb
+++ gene_pool_comp.rb
@@ -1,27 +1,37 @@
-def get_likes(likes)
-	return false unless likes
-	likes.map { |like| @user_likes << like }
-	get_likes(likes.next_page)
+def get_likes(likes, total_likes = [])
+	return total_likes unless likes
+	likes.map { |like| total_likes << like }
+	get_likes(likes.next_page, total_likes)
 end
 
-def get_comments(comments)
-	return false unless comments
+def get_comments(comments, total_comments = [])
+	return total_comments unless comments
 	comments.each do |comment|
 		if comment['message'].downcase.delete(' ').include? WINNING_PHRASE
-			@user_comments << comment['from']
+			total_comments << comment['from']
 		end
 	end
-	get_comments(comments.next_page)
+	get_comments(comments.next_page, total_comments)
 end
 
 def valid_users
-	@user_likes & @user_comments
+	@user_post_likes & @user_post_comments
 end
 
 def get_winner
 	winner = valid_users.sample
-	details = @graph.get_object(winner['id'])
-	{ name: details['name'], link: details['link'] }
+	if verify_winner(winner['id'])
+		details = @graph.get_object(winner['id'])
+		{ name: details['name'], link: details['link'] }
+	else
+		get_winner
+	end
+end
+
+def verify_winner(winner)
+	puts 'Verifying winner'
+	return true if @graph.get_connections(winner, 'likes')
+	false
 end
 
 require 'koala'
@@ -30,22 +40,19 @@ require 'dotenv'
 Dotenv.load
 
 API_KEY = ENV['API_KEY']
-OBJECT_ID = '771957389650003'
+PAGE_ID = '578972198948524'
+POST_ID = '771957389650003'
 WINNING_PHRASE = 'tackle52'
-
-@user_likes = []
-@user_comments = []
 
 Koala.config.api_version = 'v2.3'
 
 @graph = Koala::Facebook::API.new(API_KEY)
-@post = @graph.get_object(OBJECT_ID)
 
-puts 'Getting likes'
-get_likes @graph.get_connections(OBJECT_ID, 'likes')
+puts 'Getting post likes'
+@user_post_likes = get_likes(@graph.get_connections(POST_ID, 'likes'))
 
-puts 'Getting comments'
-get_comments @graph.get_connections(OBJECT_ID, 'comments')
+puts 'Getting post comments'
+@user_post_comments = get_comments(@graph.get_connections(POST_ID, 'comments'))
 
 winner = get_winner
 puts "\nOut of #{valid_users.length} valid entries, the winner is:-"
